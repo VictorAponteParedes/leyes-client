17a3408adad9cbe68cacf3d5ba589033efe29242
VictorAponteParedes <vaponte520@gmail.com>
2023-12-09 17:58:28 -0300

    Correcion: se corrige temporalmente la url del listado de abogados.

--- client/src/components/home-components.jsx
+++ client/src/components/home-components.jsx
@@ -13,7 +13,7 @@ export function Home() {
           />
 
           <div className="p-5">
-            <a href="/app/especialidad">
+            <a href="#">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 ¿Eres abogado?
               </h5>
@@ -55,7 +55,7 @@ export function Home() {
               confianza.
             </p>
             <a
-              href="#"
+              href="/app/casos/"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Ver mas!
               <svg
