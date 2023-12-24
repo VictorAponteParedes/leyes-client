b77a33af1914f82a1b6e2ceceeb2374b3f3d4548
VictorAponteParedes <vaponte520@gmail.com>
2023-12-24 18:45:05 -0300

    Actualizacion: se actualizan las clases de las navegaciones

--- client/src/components/navegaciones/navigation-caso.jsx
+++ client/src/components/navegaciones/navigation-caso.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export function NavigationCaso() {
   return (
-    <div className="flex justify-between py-4">
+    <div className="flex justify-between py-4 mx-2">
       <Link to="/casos">
-        <h2 className="font-bold text-3xl mb-4 text-gray-800 dark:text-black">
+        <h2 className="font-bold text-2xl mb-2 text-gray-800 dark:text-black">
           Sección de Caso
         </h2>
       </Link>
 
-      <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-3">
+      <button className="text-white bg-gradient-to-r bg-gray-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-black/80 font-medium rounded-lg text-sm px-3 py-1.5 text-center">
         <Link to="/casos/crear/" className="text-white">
           Crear Caso
         </Link>
